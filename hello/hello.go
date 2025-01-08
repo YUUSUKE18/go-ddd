f13044dce46e4bbcbd731a926e5f43120449267b
YUUSUKE18 <yuusuke.o.work.1018@gmail.com>
2025-01-08 23:02:14 +0900

    finish Hello World Chapter

--- hello/hello.go
+++ hello/hello.go
@@ -2,10 +2,26 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "world"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case "spanish":
+		prefix = "Hola, "
+	case "french":
+		prefix = "Bonjour, "
+	default:
+		prefix = "Hello, "
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("Jhon", "spanish"))
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -11,13 +11,13 @@ func TestHello(t *testing.T) {
 	}
 	t.Run("Saying hello to people", func(t *testing.T) {
 
-		got := Hello("Chris")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 		assertCorrectMessage(t, got, want)
 	})
 	t.Run("Say 'Hello, world' when an empty string is supplied", func(t *testing.T) {
 
-		got := Hello("")
+		got := Hello("Hoge", "French")
 		want := "Hello, world"
 
 		assertCorrectMessage(t, got, want)
